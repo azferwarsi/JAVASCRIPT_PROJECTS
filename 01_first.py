d1ec650417bb9b50e4da00fa4e6b7475fdb96ead
Azfer Warsi <azferwarsi2@gmail.com>
2026-08-31 15:52:32 +0530

    Add files via upload

---
+++ 01_first.py
@@ -0,0 +1,21 @@
+# a=10
+# name="python"
+
+# print(a,name) # this is single line comment
+
+'''
+vjwhb wdjwbd wjd
+'''
+
+"""
+sxuvwjdkhwbdwkhd
+
+
+"""
+
+address=input("Enter your address")
+print(address)
+
+num=int(input("enter number"))
+print(num)
+
